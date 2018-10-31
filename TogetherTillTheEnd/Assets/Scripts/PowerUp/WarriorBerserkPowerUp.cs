b6c4c6a9987def3b978c5e7a8a17826a78384796
punkrock1000 <antoineracinegingras@hotmail.com>
2018-10-31 00:32:39 -0400

    added warrior scripts

---
+++ TogetherTillTheEnd/Assets/Scripts/PowerUp/WarriorBerserkPowerUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorBerserkPowerUp : MonoBehaviour
+{
+
+    public Warrior playerOne;
+
+    void Start()
+    {
+        GameObject tempPlayer = GameObject.Find("Warrior");
+        playerOne = tempPlayer.GetComponent<Warrior>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "PlayerOne")
+        {
+             Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ TogetherTillTheEnd/Assets/Scripts/PowerUp/WarriorRangePowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorRangePowerUp : MonoBehaviour {
+
+    public Warrior playerOne;
+
+    void Start()
+    {
+        GameObject tempPlayer = GameObject.Find("Warrior");
+        playerOne = tempPlayer.GetComponent<Warrior>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "PlayerOne")
+        {
+            playerOne.hasRangeAtk = true;
+            Destroy(gameObject);
+        }
+
+    }
+}
