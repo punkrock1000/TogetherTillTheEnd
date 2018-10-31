4bf949db29beca1c302ba1ba878b03dd8fba57b9
punkrock1000 <antoineracinegingras@hotmail.com>
2018-10-30 21:10:19 -0400

    New mage files

---
+++ TogetherTillTheEnd/Assets/Scripts/FollowingBackGround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingBackGround : MonoBehaviour
+{
+    GameObject Cam;
+
+	void Start ()
+    {
+		Cam = FindObjectOfType<SharedCamera>().gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(Cam.transform.position.x, Cam.transform.position.y,0);
+	}
+}
